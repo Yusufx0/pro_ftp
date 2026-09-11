ff7d2e40c1a1998c382ce4f4daf8ab39eabce52d
Yusufx0 <161377573+Yusufx0@users.noreply.github.com>
2026-09-11 10:55:28 +0000

    Kod 4 esit parcada yeniden duzenlendi ve const hatasi giderildi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,18 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+// 1. İMZA YAPILANDIRMASI İÇİN PROPERTIES DOSYASINI OKUMA BÖLÜMÜ
+val keystoreProperties = Properties()
+val keystorePropertiesFile = file("keystore.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    println("Uyarı: keystore.properties dosyası bulunamadı!")
+}
+
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.[span_0](start_span)[span_0](end_span)
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -15,25 +27,34 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).[span_1](start_span)[span_1](end_span)
         applicationId = "com.ftp.pro_ftp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // You can update the following values to match your application needs.[span_2](start_span)[span_2](end_span)
+        // For more information, see: https://flutter.dev/to/review-gradle-config.[span_3](start_span)[span_3](end_span)
         minSdk = flutter.minSdkVersion
         targetSdk = 37
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
+        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION[span_4](start_span)[span_4](end_span)
+        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)[span_5](start_span)[span_5](end_span)
+        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`[span_6](start_span)[span_6](end_span)
+        // flag during build.[span_7](start_span)[span_7](end_span)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    // 2. İMZA YAPILANDIRMASI OLUŞTURMA BÖLÜMÜ
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // 3. OLUŞTURULAN İMZAYI RELEASE (SÜRÜM) İÇİN KULLANMA
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
