6f019f384acc587a52ec9c18d0710e9e8af5b2ed
Yusufx0 <161377573+Yusufx0@users.noreply.github.com>
2026-09-25 02:25:50 +0300

    Update Gradle and Kotlin plugin versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         // AdMob ve yeni nesil Gradle (9.x) ile uyumluluk için sürümler sabitlendi
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.android.tools.build:gradle:8.3.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
