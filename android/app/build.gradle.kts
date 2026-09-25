102c61b5da035166eeb5e839856056ae30f4c74e
Yusufx0 <161377573+Yusufx0@users.noreply.github.com>
2026-09-25 03:59:31 +0300

    Downgrade compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 android {
     namespace = "dev.corebyte.ftpcore"
-    compileSdk = 37
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -29,7 +29,7 @@ android {
     defaultConfig {
         applicationId = "dev.corebyte.ftpcore"
         minSdk = flutter.minSdkVersion
-        targetSdk = 37
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
